e4566652c8408c3b1cf67dd6603a043a8f0e29f1
Abdoul SAlam DIALLO <as.d17@zig.univ.sn>
2025-03-23 15:19:36 +0000

    un nouveau commit

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "guide_bacheliers";
+$host = "mysql-abdoul-salam-diallo.alwaysdata.net";
+$user = "405601_guidebach";
+$pass = "Asd781209169#";
+$dbname = "abdoul-salam-diallo_guidebacheliers";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
